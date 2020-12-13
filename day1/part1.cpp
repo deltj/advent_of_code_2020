4fec7fd00054bb3e187b5bc60c071f77b0e60bf9
deltj <deltj@outlook.com>
2020-12-12 20:39:16 -0500

    day1

---
+++ day1/part1.cpp
@@ -0,0 +1,55 @@
+/**
+ * Given an list of numbers, find two that add up to 2020 and multiply them
+ */
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <sstream>
+
+void usage()
+{
+    std::cout << "day1 <input.txt>" << std::endl;
+}
+
+int main(int argc, char *argv[])
+{
+    if(argc != 2)
+    {
+        usage();
+        return EXIT_FAILURE;
+    }
+
+    std::vector<int> numbers;
+
+    std::ifstream ifs(argv[1], std::ifstream::in);
+    std::string line;
+    int number;
+    while(std::getline(ifs, line))
+    {
+        std::istringstream iss(line);
+        iss >> number;
+        numbers.push_back(number);
+    }
+
+    ifs.close();
+
+    std::cout << "read " << numbers.size() << " numbers" << std::endl;
+
+    size_t n = numbers.size();
+    int a, b;
+    for(int i=0; i<n; ++i)
+    {
+        a = numbers[i];
+        for(int j=i+1; j<n; ++j)
+        {
+            b = numbers[j];
+
+            if(a + b == 2020)
+            {
+                std::cout << "a=" << a << ",b=" << b << ",product="
+                        << a * b << std::endl;
+            }
+        } 
+    }
+}
---
+++ day1/part2.cpp
@@ -0,0 +1,59 @@
+/**
+ * Given an list of numbers, find three that add up to 2020 and multiply them
+ */
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <sstream>
+
+void usage()
+{
+    std::cout << "day1 <input.txt>" << std::endl;
+}
+
+int main(int argc, char *argv[])
+{
+    if(argc != 2)
+    {
+        usage();
+        return EXIT_FAILURE;
+    }
+
+    std::vector<int> numbers;
+
+    std::ifstream ifs(argv[1], std::ifstream::in);
+    std::string line;
+    int number;
+    while(std::getline(ifs, line))
+    {
+        std::istringstream iss(line);
+        iss >> number;
+        numbers.push_back(number);
+    }
+
+    ifs.close();
+
+    std::cout << "read " << numbers.size() << " numbers" << std::endl;
+
+    size_t n = numbers.size();
+    int a, b, c;
+    for(int i=0; i<n; ++i)
+    {
+        a = numbers[i];
+        for(int j=i+1; j<n; ++j)
+        {
+            b = numbers[j];
+
+            for(int k=j+1; k<n; ++k)
+            {
+                c = numbers[k];
+                if(a + b + c == 2020)
+                {
+                    std::cout << "a=" << a << ",b=" << b << ",c=" << c
+                            << "product=" << a * b * c<< std::endl;
+                }
+            }
+        } 
+    }
+}
